18567522dcd2d9f5d14517349956b4183a1fb186
mac2394q <mac2394q.gmail.com>
2018-04-15 15:09:03 -0500

    dfsd

---
+++ vistas/administrador.php
@@ -0,0 +1,54 @@
+<div class="col-xs-12">
+                            <div class="box">
+                                <div class="box-header">
+                                    <h3 class="box-title">Responsive Hover Table</h3>
+                                    <div class="box-tools">
+                                        <div class="input-group">
+                                            <input type="text" name="table_search" class="form-control input-sm pull-right" style="width: 150px;" placeholder="Filtrar minutas por :">
+                                            <div class="input-group-btn">
+                                                <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div><!-- /.box-header -->
+                                <div class="box-body table-responsive no-padding">
+                                    <table class="table table-hover">
+                                        <tbody>
+                                          <tr>
+                                          <th>Estado</th>
+                                          <th>Codigo Servicio</th>
+                                          <th>Conductor</th>
+                                          <th>Vehiculo</th>
+                                          <th>Cliente</th>
+                                          <th>Ciudad origen</th>
+                                          <th>Ciudad Destino</th>
+                                          <th>Fecha Creacion</th>
+                                          <th>Fecha Fin</th>
+                                        </tr>
+                                        <tr>
+                                          <td><span class="label label-danger">Vencido</span></td>
+                                          <td>183234</td>
+                                          <td>John Doe</td>
+                                          <td>abc-123</td>
+                                          <td>cliente #123</td>
+                                            <td>Buenaventura</td>
+                                            <td>Cali</td>
+                                            <td>15-4-2018</td>
+                                            <td>...</td>
+                                        </tr>
+
+                                        <tr>
+                                          <td><span class="label label-warning">Pronto a vencer</span></td>
+                                          <td>183234</td>
+                                          <td>John Doe</td>
+                                          <td>abc-123</td>
+                                          <td>cliente #123</td>
+                                            <td>Buenaventura</td>
+                                            <td>Cali</td>
+                                            <td>15-4-2018</td>
+                                            <td>...</td>
+                                        </tr>
+                                    </tbody></table>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
+                        </div>
--- vistas/layout/general/principal.php
+++ vistas/layout/general/principal.php
@@ -1,1 +1,74 @@
-gfgdfgdf
+<div class="col-md-6">
+                    <div class="box box-solid">
+                        <div class="box-header">
+                            <h3 class="box-title">Carousel</h3>
+                        </div><!-- /.box-header -->
+                        <div class="box-body">
+                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img src="http://placehold.it/900x500/39CCCC/ffffff&amp;text=I+Love+Bootstrap" alt="First slide">
+                                        <div class="carousel-caption">
+                                            First Slide
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="http://placehold.it/900x500/3c8dbc/ffffff&amp;text=I+Love+Bootstrap" alt="Second slide">
+                                        <div class="carousel-caption">
+                                            Second Slide
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="http://placehold.it/900x500/f39c12/ffffff&amp;text=I+Love+Bootstrap" alt="Third slide">
+                                        <div class="carousel-caption">
+                                            Third Slide
+                                        </div>
+                                    </div>
+                                </div>
+                                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div>
+                        </div><!-- /.box-body -->
+                    </div><!-- /.box -->
+                </div>
+
+
+                <div class="col-md-6">
+                                            <div class="box box-solid">
+                                                <div class="box-header">
+                                                    <h3 class="box-title">Vertical Progress bars</h3>
+                                                </div><!-- /.box-header -->
+                                                <div class="box-body text-center">
+                                                    <p>By adding the class <code>.vertical</code> we achieve:</p>
+                                                    <div class="progress vertical">
+                                                        <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="height: 40%">
+                                                            <span class="sr-only">40%</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="progress vertical">
+                                                        <div class="progress-bar progress-bar-aqua" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="height: 20%">
+                                                            <span class="sr-only">20%</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="progress vertical">
+                                                        <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="height: 60%">
+                                                            <span class="sr-only">60%</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="progress vertical">
+                                                        <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="height: 80%">
+                                                            <span class="sr-only">80%</span>
+                                                        </div>
+                                                    </div>
+                                                </div><!-- /.box-body -->
+                                            </div><!-- /.box -->
+                                        </div
